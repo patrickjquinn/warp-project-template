3548cd8ec29632a462e0ff4e7609ee9659b6ff54
patrickjquinn <patrick.jm.quinn@gmail.com>
2021-08-29 17:57:32 +0100

    Added the capacitor and vite configs for the template.

---
+++ src/pages/index.svelte
@@ -0,0 +1,20 @@
+<script>
+    let name = "your first Warp project!"
+    let image = "svelte_cap.png"
+</script>
+
+<div class="main">
+    <img alt="" src={image}/>
+    <h1>Welcome to {name}!</h1>
+</div>
+
+<style>
+    .main {
+        text-align: center;
+        margin: 50px auto;
+    }
+
+    img {
+        max-width: 100%;
+    }
+</style>
