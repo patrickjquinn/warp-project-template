dbe3dd197b60e5c52f633a4e18204b6b0efbd82d
Pat Quinn <patrick.jm.quinn@gmail.com>
2021-09-01 23:34:15 +0100

    Again, remove dead default library

--- src/components/warp/index.ts
+++ src/components/warp/index.ts
@@ -5,5 +5,4 @@ export { default as Button } from './Button.svelte'
 export { default as TextInput } from './TextInput.svelte'
 export { default as VideoPlayer } from './VideoPlayer.svelte'
 export { default as Image } from './Image.svelte'
-export { default as Generic } from './Generic.svelte'
 
