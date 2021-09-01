4ea1244c283e6d8dca1fa33b72e953b61a3c8e79
Pat Quinn <patrick.jm.quinn@gmail.com>
2021-09-01 23:41:11 +0100

    Fixed more strcuture issues with the def widgets

--- src/components/warp/ScrollContainer.svelte
+++ src/components/warp/ScrollContainer.svelte
@@ -1,26 +1,8 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import { CodeMap } from '../../../modules/warp/codeMap/index'
-
-	export let css: Record<string,unknown>
 	export let id: string
-
-	let codeMap = new CodeMap('ts')
-	let styled: string
-	let rendered = false
-
-	$: {
-		if (rendered) {
-			styled = codeMap.convertCSSJSONtoInline(css, id)
-		}
-	}
-
-	onMount(async () => {
-		rendered = true
-	})
 </script>
 
-<div id="{id}" style="{styled}"><slot /></div>
+<div id="{id}"><slot /></div>
 
 <style>
 	div {
--- src/components/warp/VideoPlayer.svelte
+++ src/components/warp/VideoPlayer.svelte
@@ -1,28 +1,9 @@
 <script lang="ts">
-	import { onMount } from 'svelte'
-	import { CodeMap } from '../../../modules/warp/codeMap/index'
-
-	export let css: Record<string, unknown>
 	export let id: string
 	export let src: string
-
-	let codeMap = new CodeMap('ts')
-	let styled: string
-	let rendered = false
-
-	$: {
-		if (rendered) {
-			styled = codeMap.convertCSSJSONtoInline(css, id)
-		}
-	}
-
-	onMount(async () => {
-		rendered = true
-	})
 </script>
 
 <iframe
-	style="{styled}"
 	id="{id}"
 	width="auto"
 	height="auto"
